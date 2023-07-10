544f2392f5e3c5e3ec2b7c987cd118fd8f92547c
david onwuli <dcazicruz@icloud.com>
2023-07-10 13:45:38 +0100

    added routes for product controllers

---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema =  new Schema ({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    userName: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: false,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    cart: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Cart",
+    }],
+})
---
+++ server/routes/products.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { getAllProducts } = require("../controllers/productController");
+const {addProduct} = require("../controllers/productController");
+const {purchaseProduct} = require("../controllers/productController");
+//const {deleteProduct} = require("../controllers/productController");
+
+function productControllerRouting() {
+  const router = express.Router();
+  application.use('/', router);
+
+  router.get("/", (req, res) => {
+    getAllProducts(req, res);
+});
+  router.post("/add", (req, res)=>{
+    addProduct(req,res);
+    });
+  router.post("/purchase/:id", (req,res)=>{
+    purchaseProduct(req,res);
+    });
+  //router.delete("/delete/:id", deleteProduct);
+}
