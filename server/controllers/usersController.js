675c5546cb25dfe5fd623b2c2d1b566ce5c34675
david onwuli <dcazicruz@icloud.com>
2023-07-12 05:42:10 +0100

    added a registeruser function to the user controller

---
+++ server/controllers/usersController.js
@@ -0,0 +1,44 @@
+const User = require("../models/User");
+const path = require("path");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+    const { fname, lname, email, userName, address, password } = req.body;
+    // hashing the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // checking if user exist in db
+    old_user = await User.findOne({ email });
+    if (old_user) {
+        return res.status(400).json({ error: "Email already exists" });
+    }
+
+
+    try {
+        const newUser = await User.create({
+            fname,
+            lname,
+            email,
+            userName,
+            address,
+            password: hashedPassword,
+        });
+        res.send({ message: "User registered successfully!" });
+        console.log(newUser);
+    }
+    catch (error) {
+        res.send({ message: "An error occured. Please try again later" });
+        console.log(error);
+    }
+
+    // adding the jwt token
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    });
+};
+
+// exporting the functions
+module.exports = {
+    registerUser,}
--- server/models/User.js
+++ server/models/User.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema =  new Schema ({
-    name: {
+    fname: {
         type: String,
         required: true,
+    },
+    lname: {
+        type: String,
+        required: true
     },
     email: {
         type: String,
