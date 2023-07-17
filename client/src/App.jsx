8d43afccd6c08a35af5d16335e7035d7d1c35451
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-07-17 05:46:10 +0100

    add: functional product page with search and filtering components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PageLayOut } from "./layouts/PageLayOut";
 import HomePage from "./pages/homePage/HomePage";
-import { MainProdut } from "./components/products/MainProdut";
 import { AboutUs } from "./components/aboutUs/AboutUs";
 import { Login } from "./pages/login/Login";
 import { Register } from "./pages/register/Register";
+// import { Product } from "./Products/Products";
 // import { AuthProvider } from "./context/AuthProvider";
+import Output from "./Ouput/Output";
 
 export default function App() {
   return (
@@ -15,8 +16,8 @@ export default function App() {
           <Route path="/" element={<PageLayOut />}>
             <Route index path="/home" element={<HomePage />} />
             <Route path="/aboutus" element={<AboutUs />} />
-            <Route path="/product" element={<MainProdut />} />
           </Route>
+          <Route path="/product" element={<Output />} />
           {/* <AuthProvider></AuthProvider> */}
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
--- client/src/components/products/MainProdut.jsx
+++
@@ -1,9 +0,0 @@
-
-export const MainProdut = () => {
-  return (
-       <div className='shoppage'>
-         <h1>main product</h1>
-    </div>
-  )
-}
-
